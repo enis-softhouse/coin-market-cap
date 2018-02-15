91db2f7e9f7dddafb2918a26b78f57e2abe88ee6
Mirza Curic <mirza.curic@hotmail.com>
2018-02-15 07:55:48 +0100

    Initial commit and setup redux.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
+import { AppRoutingModule } from './app-routing.module';
+
+import { NgReduxModule, NgRedux } from '@angular-redux/store';
+import { store, IAppState } from './store';
+
+import { CryptoCurrencyListComponent } from './presentation/crypto-currency-list/crypto-currency-list.component';
+import { CryptoCurrencyDetailsComponent } from './presentation/crypto-currency-details/crypto-currency-details.component';
+import { MenuComponent } from './presentation/menu/menu.component';
+
+import { CryptoCurrencyService } from './api/crypto-currency.service';
+
+import { CryptoCurrencyActions } from './actions/crypto-currency.action';
+import { CurrencyActions } from './actions/currency.action';
 
 import { AppComponent } from './app.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    CryptoCurrencyListComponent,
+    CryptoCurrencyDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    AppRoutingModule,
+    NgReduxModule
   ],
-  providers: [],
+  providers: [
+    CryptoCurrencyService,
+    CryptoCurrencyActions,
+    CurrencyActions
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor(ngRedux:NgRedux<IAppState>){
+    ngRedux.provideStore(store);
+  }
+}
